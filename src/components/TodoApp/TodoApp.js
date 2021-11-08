c5466ac9f9dadc0ea9ead8b4e4679e73ce294697
Rashid Machingal <rashileocontact@gmail.com>
2021-11-09 03:05:38 +0530

    UI Updated

--- src/components/TodoApp/TodoApp.js
+++ src/components/TodoApp/TodoApp.js
@@ -2,15 +2,47 @@ import React, { Component } from 'react'
 import './TodoApp.css'
 
 class TodoApp extends Component {
+
+    state = {
+        input: "",
+        items: []
+    }
+
+
+    handleChange = (event) =>{
+        this.setState({
+            input: event.target.value
+        })
+    }
+
+    storeItems = (event) =>{
+        event.preventDefault()
+        const {input} = this.state;
+        const allItems = this.state.items;
+
+        allItems.push(input);
+
+        this.setState({
+            items: allItems
+        })
+    }
+
     render() {
+        const {input,items} = this.state
+
+        console.log(items);
+
         return (
             <div className="todo-container">
-                <h1>Todo App</h1>
 
-                <div className="input-section">
-                    <input type="text" />
-                    <button>Add</button>
-                </div>
+                <form className="input-section" onSubmit={this.storeItems}>
+                <h1>Todo App</h1>
+                    <input type="text" value={input} onChange={this.handleChange} placeholder="Enter Items..." /> 
+                </form>
+
+                <ul>
+                    <li>Items <i className="fas fa-trash-alt"></i></li>
+                </ul>
             </div>
         )
     }
